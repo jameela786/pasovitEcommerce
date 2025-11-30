8d94c95945405964ca82c1dd07ca0aa7db6e6488
jameela-nxtwv <jameelashaik7799@gmail.com>
2025-11-30 22:34:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 //   credentials: true
 // }));
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: 'https://pasovit-ecommerce-54f85lmqy-jameela-s-projects.vercel.app/' || 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
