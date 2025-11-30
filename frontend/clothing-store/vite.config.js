428fc9258c64ae4ccfa55342f51e0ee7e8e48743
jameela-nxtwv <jameelashaik7799@gmail.com>
2025-11-30 22:30:10 +0530

    Update vite.config.js

--- frontend/clothing-store/vite.config.js
+++ frontend/clothing-store/vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    }
-  }
+  server: { historyApiFallback: true }
 })
