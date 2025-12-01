c625c46c2f2c9587062684606889d770a0d61147
jameela786 <jameelashaik7799@gmail.com>
2025-12-01 16:05:00 +0530

    cart updated

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -51,7 +51,6 @@ exports.register = async (req, res) => {
     res.cookie('jwt', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 30 * 24 * 60 * 60 * 1000
     });
 
@@ -92,8 +91,7 @@ exports.login = async (req, res) => {
 
     res.cookie('jwt', token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "none" ,
+      secure: process.env.NODE_ENV === 'production',
       maxAge: 30 * 24 * 60 * 60 * 1000
     });
 
--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,10 @@ app.use(cookieParser());
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",
+    'http://localhost:5173',
     "https://pasovit-ecommerce-54f85lmqy-jameela-s-projects.vercel.app",
   ],
+  // origin: "*",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
